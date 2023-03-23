2f1392b7f2662aad9e44c3d321261489d553c5f9
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2023-03-23 15:17:36 +0400

    Models created

---
+++ database/migrations/2023_02_09_131012_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Common\Country;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('GIN');
+            $table->tinyText('description');
+            $table->string('UOM');
+            $table->string('category');
+            $table->foreignIdFor(Country::class);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
